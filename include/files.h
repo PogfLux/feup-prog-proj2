bcd67c94eb7b956aab8633acd59666cf7bcf6ca6
Nuno Pereira <nunoafonso2002@gmail.com>
2021-05-24 19:32:00 +0100

    Added functions for managing files

---
+++ include/files.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#define RESOURCES_PATH "./resources/"
+
+#define LEADERBOARDS_PATH "leaderboards/"
+
+#define MAZES_PATH "mazes/"
+
+class FileManager {
+
+    public:
+        static bool fileExists(const std::string& fileName, const std::string& relativePathInCwd = "");
+        static void setCWD(std::string cwd);
+        static std::vector<std::string> readFileLines(const std::string& filename, const std::string& relativePathInCwd = "");;
+
+    private:
+        FileManager(); // declare constructor private so this class can't be instanciated
+        ~FileManager();
+
+        static std::string cwd;
+};
---
+++ src/files.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "../include/files.h"
+
+std::string FileManager::cwd; // this line is needed so that cwd is not undefined
+
+bool FileManager::fileExists(const std::string& fileName, const std::string& relativePathInCwd) {
+
+    std::ifstream file(FileManager::cwd + relativePathInCwd + fileName);
+
+    if (file.is_open()) {
+        file.close();
+        return file.good(); // the file could be deleted by an external process unknown to us, by returning 'file.good()' we ensure that even if the file does get deleted, the correct status is returned.
+    }
+
+    return false; // default behavior
+};
+
+std::vector<std::string> FileManager::readFileLines(const std::string& filename, const std::string& relativePathInCwd) {
+
+    std::vector<std::string> fileLines;
+
+    std::fstream f;
+
+    f.open(FileManager::cwd + relativePathInCwd + filename, std::ios::in);
+
+    if (f.is_open()) {
+
+        // file exists, read lines
+
+        std::string line;
+
+        while (std::getline(f, line))   
+            fileLines.push_back(line);      
+    
+        f.close();
+    }
+
+    return fileLines;
+};
+
+void FileManager::setCWD(std::string newCWD) {
+    FileManager::cwd = newCWD; 
+}
